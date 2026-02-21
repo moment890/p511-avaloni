1af9de94e74f201ba6ef55211882af29bf9fd894
User <User@WIN-H77MA6T5FBT>
2026-02-21 14:49:35 +0500

    avavoni menu

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,17 +1,25 @@
+
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Diagnostics;
 using static System.Net.Mime.MediaTypeNames;
+
 
 namespace p511_890.Views {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            
-
         }
 
-        private void handleClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e) 
-        {
-            Content = "привет";
+        private void Button_OnClick(object? sender, RoutedEventArgs e) {
+            if (double.TryParse(Celsius.Text, out double C)) {
+                var F = C * (9d / 5d) + 32;
+                Fahrenheit.Text = F.ToString("0.0");
+            } else {
+                Celsius.Text = "0";
+                Fahrenheit.Text = "0";
+            }
+
         }
     }
 }
